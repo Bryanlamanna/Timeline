d663359eddc3b602af76a0a7be9c66c571f2ce81
Bryan Lamanna <lamannabryan@gmail.com>
2023-11-29 09:55:30 -0300

    add the images

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 const emojis = document.querySelectorAll('.moment-emoji-right, .moment-emoji-left');
 const moments = document.querySelector('.moment-photo');
+const closebtn = document.querySelector('.fa-circle-xmark');
+const imagem = document.querySelector('.photo');
+var album = [
+    'assets/imagem (1).jpg',
+    'assets/imagem (2).jpg',
+    'assets/imagem (3).jpg',
+    'assets/imagem (4).jpg',    
+    'assets/imagem (5).jpg',
+    'assets/imagem (6).jpg',
+    'assets/imagem (7).jpg',
+    'assets/imagem (8).jpg',
+    'assets/imagem (9).jpg',
+    'assets/imagem (10).jpg',
+    'assets/imagem (11).jpg',
+    'assets/imagem (12).jpg',
+] 
 
-for (let i = 0; i < emojis.length; i++) {
+
+closebtn.addEventListener('click', function() {
+    document.body.style.backgroundColor = 'rgb(236, 213, 213)';
+    moments.style.visibility = 'hidden';
+    moments.style.opacity = '0';
+})
+
+for (let i = 0; i < album.length; i++) {
     emojis[i].addEventListener('click', function() {
         document.body.style.backgroundColor = 'rgba(0, 0, 0, 0.481)';
+        imagem.src = album[i];
         moments.style.visibility = 'visible';
         moments.style.opacity = '1';
     });   
@@ -16,3 +40,4 @@ document.addEventListener('keydown', function(event) {
         moments.style.opacity = '0';
     }
 })
+
