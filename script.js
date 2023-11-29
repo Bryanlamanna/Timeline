be2843ea552ec87fbd92b1f7ea964a0b9c8ca904
Bryan Lamanna <lamannabryan@gmail.com>
2023-11-29 12:20:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -35,11 +35,15 @@ for (let i = 0; i < emojis.length; i++) {
     emojis[i].addEventListener('click', function() {
         document.body.style.backgroundColor = 'rgba(0, 0, 0, 0.481)';
         imagem.src = album[i];
-        imagem.style.visibility = 'visible';
-        moments.style.visibility = 'visible';
-        moments.style.opacity = '1';
+        imagem.onload = function() {
+            // Quando a imagem estiver carregada, exibe a div moments
+            imagem.style.visibility = 'visible';
+            moments.style.visibility = 'visible';
+            moments.style.opacity = '1';
+        };
     });   
 }
+
 
 document.addEventListener('keydown', function(event) {
     if (event.key === 'Escape') {
