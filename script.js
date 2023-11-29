148b1f306139715abd143d023f7ad189e51e69c2
Bryan Lamanna <lamannabryan@gmail.com>
2023-11-29 05:41:31 -0300

    initial upload

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-const dates = document.querySelectorAll('.date');
-const content = document.querySelector('.content');
+const emojis = document.querySelectorAll('.moment-emoji-right, .moment-emoji-left');
+const moments = document.querySelector('.moment-photo');
 
-dates.forEach(function(element) {
-    element.addEventListener("mouseenter", function() {
-        content.style.opacity = '1';
-    });
+for (let i = 0; i < emojis.length; i++) {
+    emojis[i].addEventListener('click', function() {
+        document.body.style.backgroundColor = 'rgba(0, 0, 0, 0.481)';
+        moments.style.visibility = 'visible';
+        moments.style.opacity = '1';
+    });   
+}
 
-    element.addEventListener("mouseleave", function() {
-        content.style.opacity = '0';
-    });
-});
-
-
-
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Escape') {
+        document.body.style.backgroundColor = 'rgb(236, 213, 213)';
+        moments.style.visibility = 'hidden';
+        moments.style.opacity = '0';
+    }
+})
